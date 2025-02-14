1838b8c04ce5a59b843bcf4147e644cc31b51018
MohamedHamouch <med10hamouch@gmail.com>
2025-02-14 12:17:58 +0100

    add methods to class admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,28 +5,33 @@ use App\Models\User;
 use App\Core\Database;
 use PDO;
 
-class Admin extends User
-{
+class Admin extends User {
     private $conn;
 
-    public function __construct($db)
-    {
+    public function __construct($db) {
         $this->conn = $db;
     }
 
-    public function banUser($userId)
-    {
+    public function banUser($userId) {
         $query = "UPDATE users SET active = false WHERE id = :userId";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
         return $stmt->execute();
     }
 
-    public function unbanUser($userId)
-    {
+    public function unbanUser($userId) {
         $query = "UPDATE users SET active = true WHERE id = :userId";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
         return $stmt->execute();
     }
+    
+    
+    public function getAllUsers() {
+        $query = 'SELECT id, avatar, username, email, phone, role, active FROM "user"';
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+
+
