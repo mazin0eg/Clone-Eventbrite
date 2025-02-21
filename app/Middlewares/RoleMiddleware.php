58d4afb5005d9f145e27dd4da9bd4f105ec56def
MohamedHamouch <med10hamouch@gmail.com>
2025-02-21 11:18:18 +0100

    add session messages in middlewares

--- app/Middlewares/RoleMiddleware.php
+++ app/Middlewares/RoleMiddleware.php
@@ -13,6 +13,8 @@ class RoleMiddleware implements MiddlewareInterface
     public function handle()
     {
         if (!isset($_SESSION['user'])) {
+            $_SESSION['error'] = "You must be logged in to access this page.";
+
             header("Location:" . ROOTURL . "/");
             exit();
         }
@@ -20,7 +22,8 @@ class RoleMiddleware implements MiddlewareInterface
         $user = unserialize($_SESSION['user']);
 
         if (!in_array($user->getRole(), $this->requiredRoles)) {
-            header("Location:" . ROOTURL . "/");
+            $_SESSION['error'] = "You do not have permission to access this page.";
+            header("Location: " . ROOTURL . "/");
             exit();
         }
     }
